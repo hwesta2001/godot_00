1cd5ebce2119b44e29eeb10414d8b94911dd5585
Hasan YILMAZ <hasanhw@gmail.com>
2023-09-19 09:10:34 +0300

    hh

--- scripts/GLOBALS.gd
+++ scripts/GLOBALS.gd
@@ -2,3 +2,14 @@ extends Node
 
 var HEXES: Array[Node3D]
 var HEXES_POSITIONS: Array[Vector3]
+
+func NearestHexPos(node:Node3D):
+	var pos = HEXES_POSITIONS[0]
+	var dist = node.position.distance_squared_to(HEXES_POSITIONS[0])
+	for x in HEXES_POSITIONS:
+		if node.position.distance_squared_to(x) < dist:
+			pos = x
+			dist = node.position.distance_squared_to(x)
+	return dist
+
+
