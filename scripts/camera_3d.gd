4447e9abda8ad6b9e31a9601ed51f6b6c3657484
Hasan YILMAZ <hasanhw@gmail.com>
2023-09-21 10:10:38 +0300

    ca

--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -1,9 +1,10 @@
-extends Camera3D
+extends Node3D
 
-#@export var cam_hight_speed = 15.0
+@export var cam_hight_speed = 20.0
 @export var speed = 13.0
 @export var cam_rotX_speed = 1.1
 @export var cam_rotY_speed = 1.1
+@onready var cam = $Camera3D
 
 var rotX: float = 0
 var rotY: float = 0
@@ -11,18 +12,19 @@ var rotY: float = 0
 signal ray_cam_mouse(node:Node3D)
 
 func _process(delta):
-	translate(move()*delta*speed)
+	position+=(move()*delta*speed)
 	position.y = clampf(position.y,-1,20)
+	moveUp(delta)
 
-
-#func moveUp(delta):
-#	if Input.is_action_pressed("cam_up"):
-#		if position.y<20:
-#			position.y += delta*cam_hight_speed
-#	if Input.is_action_pressed("cam_down"):
-#		if position.y>1:
-#			position.y -= delta*cam_hight_speed
-#	position.y = clampf(position.y,1,20)
+func moveUp(delta):
+	var diry=position.y
+	if Input.is_action_just_pressed("cam_up"):
+		diry-=cam_hight_speed
+	if Input.is_action_just_pressed("cam_down"):
+		diry+=cam_hight_speed
+		
+	position.y=lerpf(position.y,diry,delta)
+	position.y = clampf(position.y,1,20)
 
 
 func move():
@@ -42,8 +44,8 @@ func _input(event):
 	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		rotX -= event.relative.y * cam_rotX_speed
 		rotY -= event.relative.x * cam_rotY_speed
-		rotX=clampf(rotX,-89.99,60)
-		rotation.x=deg_to_rad(rotX)
+		rotX=clampf(rotX,-45,70)
+		cam.rotation.x=deg_to_rad(rotX)
 		rotation.y=deg_to_rad(rotY)
 
 
